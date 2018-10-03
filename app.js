e686216cc9ec6cc8da9fc4a0607f3af2d82c3566
Rakesh <malakar.rakesh1993@gmail.com>
2018-10-03 22:19:24 +0545

    progresses

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 //database connection
 var mongodb = require('mongodb');
 var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
-
+var ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 
 //configure body-parser for express
@@ -20,45 +20,192 @@ app.use(bodyParser.json());
 
 var T = new Twitter(config.keys);
 
+/* user sign-up end-point */
+app.post('/signup', function(req, res){
+    dbConn.then( function (database) {
+            var dbo = database.db("twitter_search");
+            let user_data = {
+              username: req.body.username,
+              password: req.body.password,
+              email: req.body.email,
+              firstname: req.body.fname,
+              lastname: req.body.lname,
+              contact:'',
+              description:'',
+              skills:'',
+              address:'',
+              hobby:'',
+              organization_address:'',
+              favorite_food:'',
+              skills:'',
+              image_url:'',
+            }
+            dbo.collection('users').insertOne(user_data, function (err, records) {
+              if(err) throw err;
+              var token = jwt.sign({ user_id:records.ops[0]._id }, config.secret_key, {
+                expiresIn: 86400
+              });
+              res.status(200).json({token: token, msg: 'authenticated'});
+            });
+
+    }).catch(function(e){console.log(e)})
+});
+
+/* user login end-point */
 app.post('/user_authenticate', function(req, res){
 
     dbConn.then( function (database) {
             var dbo = database.db("twitter_search");
 
-            dbo.collection('users').countDocuments({ username: req.body.username,  password: req.body.password})
-            .then((count) => {
-                if (count > 0) {
-                  var token = jwt.sign({ username: req.body.username }, config.secret_key, {
+            dbo.collection('users').findOne({ username: req.body.username,  password: req.body.password})
+            .then((user) => {
+  
+                if (!user){
+                  return res.status(401).json({token: null,msg: 'not authenticated'});
+                }
+                else {
+                  var token = jwt.sign({ user_id: user._id }, config.secret_key, {
                     expiresIn: 86400
                   });
                   res.status(200).json({token: token, msg: 'authenticated'});
-              } else {
-                  res.status(401).json({token: null,msg: 'not authenticated'});
-              }
+                }
           }).catch(function(e){console.log(e)});
 
     }).catch(function(e){console.log(e)})
 });
 
-/* search api end-point */
-app.get('/api/:keyword', verifyToken, function(req, res) {
-    console.log(req.userId);
-    var keyword = req.params.keyword;
+/* user edit profile end-point */
+app.post('/editprofile', verifyToken, function(req, res){
 
+    dbConn.then( function (database) {
+            var dbo = database.db("twitter_search");
+            let update_data = {
+              firstname : req.body.fname, 
+              lastname : req.body.lname, 
+              description : req.body.desc,
+              address : req.body.add,
+              contact : req.body.contact,
+              organization_address : req.body.orgAdd,
+              hobby : req.body.hobby,  
+              skills : req.body.skills, 
+              hobby : req.body.hobby, 
+              favorite_food : req.body.favFood,
+              image_url: req.body.imgUrl 
+            };
+            dbo.collection('users').updateOne(
+              { _id: ObjectId(req.userId) }, 
+              { $set: update_data }, 
+              function(err, results){
+                if(results && results.result.n > 0) {
+                  res.status(200).json({msg: 'updated', data: update_data });
+                }
+              });
+    }).catch(function(e){console.log(e)})
+});
+
+/* user fetch profile end-point */
+app.post('/fetchProfile', verifyToken, function(req, res){
+
+    dbConn.then( function (database) {
+            var dbo = database.db("twitter_search");
+            dbo.collection('users').findOne(
+              { _id: ObjectId(req.userId) }, function(err, result) {
+                if(err) throw err;
+                res.status(200).json({msg: 'found', data:result});
+              });
+    }).catch(function(e){console.log(e)})
+});
+
+/* search api end-point */
+app.post('/api/', verifyToken, function(req, res) {
+  console.log(req.body);
+    var keyword = req.body.keyword;
     // Set up your search parameters
     var params = {
       q: keyword,
-      count: 100
+      count: 20
     }
+    if(req.body.maxID) {
+      params.max_id = req.body.maxID;
+      params.include_entities = 'true';
+      params.result_type = 'mixed';
+    }
+
+    if(req.body.sinceID) {
+      params.since_id = req.body.sinceID;
+      params.include_entities = 'true';
+      params.result_type = 'mixed';
+    }
+    console.log(params);
 
     T.get('search/tweets', params, function(err, data, response) {
       if(!err){
-        res.json(data.statuses);
+        res.json(data);
       } else {
       console.log(err);
       }
     })
-})
+});
+
+/* favorite tweet */
+app.post('/favTweet/', verifyToken, function(req, res) {
+    let tweet_id = {id: req.body.id};
+    dbConn.then( function (database) {
+            var dbo = database.db("twitter_search");
+            let update_data = {
+              'favorite_tweets': tweet_id
+            }
+            dbo.collection('users').updateOne(
+              { _id: ObjectId(req.userId) }, 
+              { $push: update_data }, 
+              function(err, results){
+                if(results && results.result.n > 0) {
+                  res.status(200).json({msg: 'updated', data: update_data });
+                }
+              });
+    }).catch(function(e){console.log(e)})
+});
+
+app.post('/fetchSingleTweet/', verifyToken, function(req, res) {
+  var params = {
+    id: req.body.tweet_id
+  }
+  
+  T.get('statuses/show', params, function(err, data, response) {
+      if(!err){
+        res.json(data);
+      } else {
+      console.log(err);
+      }
+    })
+});
+
+app.post('/fetchFavoriteTweets/', verifyToken, function(req, res) {
+  var favorites = [];
+  dbConn.then( function (database) {
+            var dbo = database.db("twitter_search");
+            dbo.collection('users').findOne(
+              { _id: ObjectId(req.userId) }, function(err, result) {
+                if(err) throw err;
+                if(!result.hasOwnProperty('favorite_tweets')) {
+                  res.status(404).json({msg:'record not found'});
+                } 
+                else {
+                  result.favorite_tweets.forEach(function (tweet) {
+                    T.get('statuses/show', {id: tweet.id}, function(err, data, response) {
+                      if(!err){
+                        favorites.push(data);
+                      } else {
+                        console.log(err);
+                      }
+                    });
+                  });
+                  console.log(favorites); 
+                  // res.status(200).json({msg:'success', data:favorites});
+                }
+              });
+    }).catch(function(e){console.log(e)})
+});
 
 /* server intialization */
 var server = app.listen(4444, function(){
--- verifyToken.js
+++ verifyToken.js
@@ -1,7 +1,6 @@
 var jwt = require('jsonwebtoken');
 var config = require('./config');
 function verifyToken(req, res, next) {
-
   var token = req.body.token || req.query.token || req.headers['authorization'];
   if (!token)
     return res.status(403).send({ auth: false, message: 'No token provided.' });
@@ -9,7 +8,7 @@ function verifyToken(req, res, next) {
     if (err)
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     // if everything good, save to request for use in other routes
-    req.userId = decoded.username;
+    req.userId = decoded.user_id;
     next();
   });
 }
